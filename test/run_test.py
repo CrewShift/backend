5c6d9dd04232a58e056db93d20b0644416b3939a
lacho-georgiev <lachezar.v.georgiev@gmail.com>
2025-03-30 02:30:26 +0200

    notifications correct testing

--- test/run_test.py
+++ test/run_test.py
@@ -19,7 +19,7 @@ def add_time_offset(time_str: str, offset_minutes: int) -> str:
 def update_schedule_for_test_user():
     # Test user id.
     user_id = "6M7nQsnYefOHfnDQI94iWJUsUz53"
-    # user_id = "hW1M3oKAxOPPKZfUxypq4nsD9HI3"
+    # user_id = "hW1M3oKAxOPPKZfUxypq4nsD9HI3" # old test
 
     # Load the full JSON data from the JSON file.
     with open("example-schedule.json", "r") as f:
